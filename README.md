38e11acdb46d4a6ac8bde355c102e78449fee154
Scott Gress <scott@pigandcow.com>
2014-02-22 02:23:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular on Sails
+
+Just a plain 'ol skeleton v0.10 Sails app with [Angular Seed](https://github.com/angular/angular-seed) in the assets folder, layout.ejs and homepage.ejs.
---
+++ tasks/config/less.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+
+	grunt.config.set('less', {
+		dev: {
+			files: [{
+				expand: true,
+				cwd: 'assets/styles/',
+				src: ['*.less'],
+				dest: '.tmp/public/styles/',
+				ext: '.css'
+			}]
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-less');
+};
